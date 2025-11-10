d77ed93ce828773fee35c562bf1ceae5a8b7326b
CamiloAndres234 <camilo.alarconm@uniagustiniana.edu.co>
2025-11-10 18:35:47 +0000

    Cuadrado

--- SumadeNumeros.js
+++ SumadeNumeros.js
@@ -1,5 +1,5 @@
 function SumarDosNumeros(num1,num2){
-    console.log("La suma es igual a: " + (num1 + num2))
+    console.log("La suma es igual a: " + (num1 + num2));
 }
 
-SumarDosNumeros(5, 3)
+SumarDosNumeros(5, 3);
