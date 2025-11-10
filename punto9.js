8b42d3cf73048a966bfc5f289195bcb5f02b0d38
Laura V Cardona <58284758+laura3212401957@users.noreply.github.com>
2025-11-10 19:38:32 +0000

    addicion ejercicios

---
+++ punto9.js
@@ -0,0 +1,7 @@
+array=[
+    {id:1, nombre:"natalia", edad:19},
+    {id:2, nombre:"veronica", edad:20},
+    {id:3, nombre:"samuel", edad:22},
+    {id:4, nombre: "laura", edad:18}
+]
+console.log(array)
